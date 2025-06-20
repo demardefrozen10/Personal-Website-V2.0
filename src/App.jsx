541b5862c6a666c89b9082df25081f51c4c49a63
Jason Deng <jasondeng107@gmail.com>
2025-06-20 10:51:40 -0400

    fix stuff

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import Navigation from './Navigation.jsx';
 import Footer from './Footer.jsx';
-export default function App() {
-  return <>
-    <Navigation />
-      <Footer/>
 
-  </>
+export default function App() {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Navigation />
+      <div className="flex-1">
+      </div>
+      <Footer />
+    </div>
+  );
 }
